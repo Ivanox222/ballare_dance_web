54e385e46f51a2bb775083335c65ab44b34e2f6d
EdwinAndrei <159513149+EdwinAndrei@users.noreply.github.com>
2025-12-09 20:34:04 -0600

    cambios fotos

--- src/data/pages/hombre.js
+++ src/data/pages/hombre.js
@@ -2,31 +2,29 @@ export const getHombresData = () => {
     return {
         galeriaHombres: {
             ropa: [
-                { title: "Corset Hombre Azul", description: "Modelo clásico", price: "$45", imgUrl: "https://picsum.photos/id/125/400/" },
-                { title: "Corset Negro Pro", description: "Alta resistencia", price: "$60", imgUrl: "https://picsum.photos/id/100/400/" },
-                {title: "Capezio Maillot", description:"Este leotardo forrado cuenta con una clásica parte delantera y trasera", price:"2000 L", imgUrl:"https://picsum.photos/id/100/400/"},
-                {title: "Pantalón deportivo", description:"Manténgase abrigado y luzca genial con nuestro pantalón", price:"1000 L", imgUrl:"https://picsum.photos/id/100/400/"}
+                { title: "Corset Hombre Azul", description: "Modelo clásico", price: "1100 L", imgUrl: "/assets/azul.jpg" },
+                { title: "Corset Negro Pro", description: "Chaleco de corsé para hombre", price: "1400 L", imgUrl: "/assets/cort.jpg" },
+                {title: "Capezio Maillot", description:"Capezio Maillot de manga corta Tactel para hombre", price:"2000 L", imgUrl:"/assets/capp.jpg"},
+                {title: "Pantalón deportivo", description:"Pantalones de baile latino para hombre y niño", price:"780 L", imgUrl:"/assets/depo.jpg"},
+                {title: "speerise Body ", description:"speerise Body de elastano para hombre", price:"670 L", imgUrl:"/assets/body.jpg"},
+                {title: "pantalones de rumba", description:"Pantalones de baile para hombre a rayas", price:"1300 L", imgUrl:"/assets/rayas.jpg"},
             ],
             zapatos: [
-                { title: "Zapato Danza Hombre", description: "Super ligero", price: "$40", imgUrl: "https://picsum.photos/id/199/400/" },
-                { title: "Zapato Danza Hombre", description: "Super ligero", price: "$40", imgUrl: "https://picsum.photos/id/199/400/" },
-                { title: "Zapato Danza Hombre", description: "Super ligero", price: "$40", imgUrl: "https://picsum.photos/id/199/400/" },
-                { title: "Zapato Danza Hombre", description: "Super ligero", price: "$40", imgUrl: "https://picsum.photos/id/199/400/" }
+                { title: "Zapatos de ballet profesionales", description: "Super ligero", price: "2000 L", imgUrl: "/assets/bh.jpg" },
+                { title: "Latin Social Waltz", description: "Diseñados para múltiples estilos de baile", price: "4000 L", imgUrl: "/assets/salsa.jpg" },
+                { title: "Zapatos de baile de jazz", description: "Zapatos de baile con amortiguación", price: "870 L", imgUrl: "/assets/jazzhom.jpg" }
             ],
 
            Suspensores: [
-                {title:"Suspensor Para Hombres", description:"Suspensor para hombre, elaborado en algodón o spandex. Diseñado para envolver la anatomía masculina", price:"500 L",imgUrl:"https://picsum.photos/id/199/400/"},
-                {title:"Suspensor Para Hombres", description:"Suspensor para hombre elaborado con lino para un mejor confort ", price:"600 L",imgUrl:"https://picsum.photos/id/199/400/"},
-                {title:"Suspensor Para Hombres", description:"Suspensor para hombre elaborado con satén para un mejor contorno ", price:"1000 L",imgUrl:"https://picsum.photos/id/199/400/"},
-                {title:"Suspensor Para Hombres", description:"Suspensor para hombre elaborado con satén para un mejor contorno ", price:"1000 L",imgUrl:"https://picsum.photos/id/199/400/"},
-            ],
+                {title:"Suspensor Para Hombres", description:"Suspensor para hombre, elaborado en algodón o spandex.", price:"500 L",imgUrl:"/assets/dep.jpg"},
+                {title:"Suspensorio Ropa Interior", description:"Suspensor para hombre elaborado con lino para un mejor confort ", price:"600 L",imgUrl:"/assets/bc.jpg"},
+                {title:"Suspensor Para Hombres Multicolores", description:"Suspensor para hombre elaborado con satén para un mejor contorno ", price:"1200 L",imgUrl:"/assets/prive.jpg"},
+                {title:"Calvin Klein Suspensorio", description:"Mejora la silueta ", price:"1300 L",imgUrl:"/assets/calvink.jpg"},
+                {title:"Ballet Comprar", description:"Ballet Clásico Suspensor Ballet", price:"1800 L",imgUrl:"/assets/suclasico.jpg"},
+                {title:"INTERMEZZO", description:"ROPA INTERIOR BALLET PARA HOMBRE COLOR BEIGE", price:"1900 L",imgUrl:"/assets/griss.jpg"}
+            ]
 
-          Mallas: [
-            {title:"Malla Para Hombres", description:"Malla para hombre, elaborado en algodón o spandex. Diseñado para envolver la anatomía masculina", price:"700 L",imgUrl:"https://picsum.photos/id/199/400/"},
-            {title:"Malla Para Hombres", description:"Malla para hombre elaborado con lino para un mejor confort ", price:"800 L",imgUrl:"https://picsum.photos/id/199/400/"},
-            {title:"Malla Para Hombres", description:"Malla para hombre elaborado con satén para un mejor contorno ", price:"1200 L",imgUrl:"https://picsum.photos/id/199/400/"},
-            {title:"Malla Para Hombres", description:"Malla para hombre elaborado con satén para un mejor contorno ", price:"1200 L",imgUrl:"https://picsum.photos/id/199/400/"},
-          ]
+          
         }
     };
 };
--- src/data/pages/mujeres.js
+++ src/data/pages/mujeres.js
@@ -2,30 +2,38 @@ export const getMujeresData = () => {
     return {
         galeriaMujeres: {
             ropa: [
-                { title: "Corset Rosa Pastel", description: "Elegante y suave", price: "459 L", imgUrl: "https://picsum.photos/id/201/400/" },
-                { title: "Corset Ballet Premium", description: "Ideal para danza", price: "900 L", imgUrl: "https://picsum.photos/id/202/400/" },
-                { title: "bustiers", description: "Ideal para danza", price: "790 L", imgUrl: "https://picsum.photos/id/202/400/" },  
-                { title: "Corset Leotard", description: "Contempla nuestro maillot con corsé.", price: "1990 L", imgUrl: "https://picsum.photos/id/202/400/" }, 
+                { title: "Corset Rosa Pastel", description: "Elegante y suave", price: "459 L", imgUrl: "/assets/corrosa.jpg" },
+                { title: "Corset Ballet Premium", description: "Ideal para danza", price: "900 L", imgUrl: "/assets/premium.jpg" },
+                { title: "Bustiers", description: "Ideal para danza", price: "790 L", imgUrl: "/assets/bustiers.jpg" },  
+                { title: "Corset Leotard", description: "Contempla nuestro maillot con corsé.", price: "1990 L", imgUrl: "/assets/leotard.jpg" },
+                { title: "Crop Top", description: "Un toque de sofisticación para tus ensayos.", price: "459 L", imgUrl: "/assets/crop.jpg" },
+                { title: "Falda Acuarela", description: "Un diseño fresco y romántico para tus ensayos.", price: "459 L", imgUrl: "/assets/acuarela.jpg" }, 
             ],
             zapatos: [
-                { title: "Zapatilla Rosa Ballet", description: "Ultra flexible", price: "450 L", imgUrl: "https://picsum.photos/id/233/400/" },
-                { title: "Zapatilla de Lona Dancefield", description: "Zapatilla de Lona Dancefield", price: "890 L", imgUrl: "https://picsum.photos/id/233/400/" },
-                { title: "Botin de Jazz Dancefield", description: "Lo mejor del mercado.", price: "1200 L", imgUrl: "https://picsum.photos/id/233/400/" },
-                { title: "Puntas Capezio Contempora", description: "Uno de los modelos más populares", price: "2500 L", imgUrl: "https://picsum.photos/id/233/400/" }
+                { title: "Zapatilla Blancas Ballet", description: "Ultra flexible", price: "450 L", imgUrl: "/assets/blancas.jpg" },
+                { title: "Zapatilla de Jazz", description: "Zapatilla Jazz de cuero", price: "890 L", imgUrl: "/assets/tap.jpg" },
+                { title: "Botin de Jazz Dancefield", description: "Lo mejor del mercado.", price: "1200 L", imgUrl: "/assets/jazz.jpg" },
+                { title: "Puntas Capezio Contempora", description: "Uno de los modelos más populares", price: "2500 L", imgUrl: "/assets/capezio.jpg" },
+                { title: "Calentadores de ballet", description: "mantén tus músculos calientes y flexibles", price: "2500 L", imgUrl: "/assets/calentador.jpg" },
+                { title: "Capezio Zapatilla Dans Fero", description: "Suela dividida con sistema de soporte de arco", price: "2500 L", imgUrl: "/assets/tennis.jpg" }
             ],
 
             Faldas: [
-                { title: "Tutu Clásico", description: "Perfecto para presentaciones", price: "1000 L", imgUrl: "https://picsum.photos/id/250/400/" },
-                { title: "Falda de Ballet", description: "Ligera y cómoda", price: "900 L", imgUrl: "https://picsum.photos/id/251/400/" },
-                { title: "Tutú Romántico", description: "Tu Mejor Opcion", price: "1570 L", imgUrl: "https://picsum.photos/id/251/400/" },
-                { title: "Tutú Balanchine", description: "Te permite Movimientos Mas Livianos", price: "1400 L", imgUrl: "https://picsum.photos/id/251/400/" }
+                { title: "Tutu Clásico", description: "Perfecto para presentaciones", price: "1000 L", imgUrl: "/assets/clasico.jpg" },
+                { title: "Falda de Ballet", description: "Tu mejor opcion Ligera y cómoda", price: "900 L", imgUrl: "/assets/falda2.jpg" },
+                { title: "Tutú Romántico", description: "La quintaesencia y la fragilidad del ballet", price: "1570 L", imgUrl: "/assets/romantico.jpg" },
+                { title: "Tutú Balanchine", description: "Te permite Movimientos Mas Livianos", price: "1400 L", imgUrl: "/assets/balanchine.jpg" },
+                { title: "Floral Skirt", description: "Una delicada falda estilo wrap para ballet", price: "1400 L", imgUrl: "/assets/floral.jpg" },
+                { title: "Falda Floral", description: "Estampado suave, movimiento fluido.", price: "1400 L", imgUrl: "/assets/faldablancaazul.jpg" }
 
             ],
             Sujetadores:[
-                { title: "Sujetador Deportivo", description: "Soporte máximo", price: "200 L", imgUrl: "https://picsum.photos/id/260/400/" },
-                { title: "Sujetador de Encaje", description: "Cómodo y elegante", price: "650 L", imgUrl: "https://picsum.photos/id/261/400/" },
-                { title: "Sujetador de Ballet", description: "Diseñado para bailarinas", price: "470 L", imgUrl: "https://picsum.photos/id/251/400/" },
-                { title: "Top de Danza", description: "Ajuste perfecto", price: "890 L", imgUrl: "https://picsum.photos/id/251/400/" }
+                { title: "Sujetador Deportivo", description: "Soporte máximo", price: "200 L", imgUrl: "/assets/sujetador.jpg"},
+                { title: "Sujetador de Encaje", description: "Cómodo y elegante", price: "650 L", imgUrl: "/assets/encaje.jpg" },
+                { title: "Sujetador de Ballet", description: "Diseñado para bailarinas", price: "470 L", imgUrl: "/assets/sujetadordeballet.jpg" },
+                { title: "Top de Danza", description: "Ajuste perfecto", price: "890 L", imgUrl: "/assets/top.jpg" },
+                { title: "Sujetador De ballet Azul", description: "Ajuste perfecto para movimientos perfectos", price: "890 L", imgUrl: "/assets/sujetadorazul.jpg" },
+                { title: "Sujetador Negro ", description: "Ajuste perfecto", price: "890 L", imgUrl: "/assets/sunegro.jpg" }
             ]
         }
     };
